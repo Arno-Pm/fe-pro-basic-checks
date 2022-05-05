fe5206f01b181545e9c40dc8ac80f7c519702789
Arno-Pm <70451630+Arno-Pm@users.noreply.github.com>
2022-05-05 15:39:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@
  * @param {string} string
  * @returns {string}
  */
-export const capitalizeString = 0;
+export const capitalizeString = (string) =>  string.split(' ').map((NewItem) => (NewItem.slice(0, 1).toUpperCase() + NewItem.slice(1))).join(' ');
 
 /**
  * Должна быть function declaration
  * @param {string} string
  * @returns {string}
  */
-export const fenceString = 0;
+export const fenceString = (string) {
+    return string.split('').map((NewItem, index) => {
+      if (index % 2 === 0) {
+        return NewItem.toLowerCase();
+      } else {
+        return NewItem.toUpperCase();
+      }
+    }).join('')
+  };
 
 /**
  * Должна быть function expression
@@ -18,7 +26,24 @@ export const fenceString = 0;
  * @param {string} string
  * @returns {string}
  */
-export const reducerIf = 0;
+export const reducerIf = function(action, string){
+    switch(action){
+        case 'uppercase':
+            return string.toUpperCase();
+        case 'lowercase':
+            return string.toLowerCase();
+        case 'fence':
+            return string.split('').map((NewItem, index) => {
+                if (index % 2 === 0) {
+                    return NewItem.toLowerCase();
+                  } else {
+                    return NewItem.toUpperCase();
+                  }
+                }).join('');
+        case 'capitalize': return string.split(' ').map((NewItem) => (NewItem.slice(0, 1).toUpperCase() + NewItem.slice(1))).join(' ')
+            default: return string;    
+            }
+    }
 
 /**
  * Стрелочная
@@ -26,28 +51,61 @@ export const reducerIf = 0;
  * @param {string} string
  * @returns {string}
  */
-export const reducerSwitch = 0;
+export const reducerSwitch = (action, string) => {
+    switch (action) {
+      case 'uppercase': 
+            return string.toUpperCase();
+      case 'lowercase':
+            return string.toLowerCase();
+      case 'fence':
+            return string.split('').map((NewItem, index) => {
+        if (index % 2 === 0) {
+          return NewItem.toLowerCase();
+        } else {
+          return NewItem.toUpperCase();
+        }
+      }).join('');
+      case 'capitalize': return string.split(' ').map((NewItem) => (NewItem.slice(0, 1).toUpperCase() + NewItem.slice(1))).join(' ');
+      default: return string;
+    }
+  };;
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsForOf = 0;
+export const consoleLoggerWordsForOf = (string) => {
+    for (let name of string) {
+      console.log(name);
+    }
+  };
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsFor = 0;
+export const consoleLoggerWordsFor =  (string) => {
+    for (let i = 0; i < string.length; i++) {
+      console.log(string[i]);
+    }
+  };;
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsWhile = 0;
+export const consoleLoggerWordsWhile = (string) => {
+    let i = 0;
+    while (i < string.length) {
+      console.log(string[i]);
+      i++;
+    }
+  };
 
 /**
  Стрелочная
  * @param {string} string
  */
-export const consoleLoggerWordsSplit = 0;
+export const consoleLoggerWordsSplit = (string) => {
+    string.split('').forEach((NewItem) => console.log(NewItem));
+  };; 
